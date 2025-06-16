7cf8ae248844d7b5fd8eb0101efdbd0ecc5b31c9
Joykinuthia <joyrosekinuthia@gmail.com>
2025-06-16 22:35:07 +0300

    Feature: Import the required libraries

---
+++ pizza-api-challenge/server/app.py
@@ -0,0 +1,4 @@
+from flask import Flask
+from server.config import Config
+from server.models import db
+from flask_migrate import Migrate
--- pizza-api-challenge/server/models/__init__.py
+++ pizza-api-challenge/server/models/__init__.py
@@ -1,3 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+# This file initializes the SQLAlchemy instance for the application.
