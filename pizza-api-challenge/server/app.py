7c616bfecb9720727a02f0204242792290415881
Joykinuthia <joyrosekinuthia@gmail.com>
2025-06-16 22:36:59 +0300

    Feature: Create and configure the Flask application

--- pizza-api-challenge/server/app.py
+++ pizza-api-challenge/server/app.py
@@ -2,3 +2,10 @@ from flask import Flask
 from server.config import Config
 from server.models import db
 from flask_migrate import Migrate
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
+    Migrate(app, db)
