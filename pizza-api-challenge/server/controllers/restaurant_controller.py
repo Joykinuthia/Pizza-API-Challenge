1bc11c17cb3e3b5d014cfd5806888eb5f0c1d88b
Joykinuthia <joyrosekinuthia@gmail.com>
2025-06-16 23:09:31 +0300

    Feature: Create a Blueprint for the restaurant controller

--- pizza-api-challenge/server/controllers/restaurant_controller.py
+++ pizza-api-challenge/server/controllers/restaurant_controller.py
@@ -1,3 +1,6 @@
 from flask import Blueprint, jsonify, request
 from server.models.restaurant import Restaurant
 from server.models import db
+
+# Create a Blueprint for the restaurant controller
+restaurant_bp = Blueprint('restaurant_bp', __name__)
