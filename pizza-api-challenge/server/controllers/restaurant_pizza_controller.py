b7f696b13740965b75e01ca21cf70cb2dff221e9
Joykinuthia <joyrosekinuthia@gmail.com>
2025-06-16 23:33:30 +0300

    Feature: Add a pizza to a restaurant

--- pizza-api-challenge/server/controllers/restaurant_pizza_controller.py
+++ pizza-api-challenge/server/controllers/restaurant_pizza_controller.py
@@ -8,3 +8,35 @@ from server.models.pizza import Pizza
 restaurant_pizza_bp = Blueprint('restaurant_pizza_bp', __name__)
 # Route to add a pizza to a restaurant
 @restaurant_pizza_bp.route('/restaurant_pizzas', methods=['POST'])
+# Add a pizza to a restaurant
+def create_restaurant_pizza():
+    data = request.get_json()
+
+    price = data.get('price')
+    pizza_id = data.get('pizza_id')
+    restaurant_id = data.get('restaurant_id')
+
+    rp = RestaurantPizza(price=price, pizza_id=pizza_id, restaurant_id=restaurant_id)
+
+    if not rp.is_valid():
+        return jsonify({"errors": ["Price must be between 1 and 30"]}), 400
+
+    db.session.add(rp)
+    db.session.commit()
+
+    return jsonify({
+        "id": rp.id,
+        "price": rp.price,
+        "pizza_id": rp.pizza.id,
+        "restaurant_id": rp.restaurant.id,
+        "pizza": {
+            "id": rp.pizza.id,
+            "name": rp.pizza.name,
+            "ingredients": rp.pizza.ingredients
+        },
+        "restaurant": {
+            "id": rp.restaurant.id,
+            "name": rp.restaurant.name,
+            "address": rp.restaurant.address
+        }
+    }), 201
