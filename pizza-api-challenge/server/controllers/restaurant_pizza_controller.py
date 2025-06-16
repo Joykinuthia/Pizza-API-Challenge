4a4aaebc1d49fb3a8a2b24d8b661105966b2f156
Joykinuthia <joyrosekinuthia@gmail.com>
2025-06-16 23:30:15 +0300

    Feature: Create a Blueprint for the restaurant  pizza controller

--- pizza-api-challenge/server/controllers/restaurant_pizza_controller.py
+++ pizza-api-challenge/server/controllers/restaurant_pizza_controller.py
@@ -3,3 +3,6 @@ from server.models import db
 from server.models.restaurant_pizza import RestaurantPizza
 from server.models.restaurant import Restaurant
 from server.models.pizza import Pizza
+
+# Create a Blueprint for the restaurant pizza controller
+restaurant_pizza_bp = Blueprint('restaurant_pizza_bp', __name__)
