f4810a8b6fe048027e8b6c6821d2266d59131530
Joykinuthia <joyrosekinuthia@gmail.com>
2025-06-16 22:54:15 +0300

    Feature: Register blueprints for the application

--- pizza-api-challenge/server/app.py
+++ pizza-api-challenge/server/app.py
@@ -3,9 +3,17 @@ from server.config import Config
 from server.models import db
 from flask_migrate import Migrate
 
+# Create and configure the Flask application
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
     db.init_app(app)
     Migrate(app, db)
+
+
+     # Register blueprints
+    
+    from server.controllers.restaurant_controller import restaurant_bp
+    from server.controllers.pizza_controller import pizza_bp
+    from server.controllers.restaurant_pizza_controller import restaurant_pizza_bp
