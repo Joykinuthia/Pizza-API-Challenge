5f3285b060fb7b8b9a50ef2d34cab034733dd78e
Joykinuthia <joyrosekinuthia@gmail.com>
2025-06-16 23:31:57 +0300

    Feature: Route  to add a pizza to a restaurant

--- pizza-api-challenge/server/controllers/restaurant_pizza_controller.py
+++ pizza-api-challenge/server/controllers/restaurant_pizza_controller.py
@@ -6,3 +6,5 @@ from server.models.pizza import Pizza
 
 # Create a Blueprint for the restaurant pizza controller
 restaurant_pizza_bp = Blueprint('restaurant_pizza_bp', __name__)
+# Route to add a pizza to a restaurant
+@restaurant_pizza_bp.route('/restaurant_pizzas', methods=['POST'])
