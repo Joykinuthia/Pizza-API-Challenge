8589906dd49c5940af77b6005ce4f56b58195880
Joykinuthia <joyrosekinuthia@gmail.com>
2025-06-16 23:26:53 +0300

    Feature: Route to get all pizzas

--- pizza-api-challenge/server/controllers/pizza_controller.py
+++ pizza-api-challenge/server/controllers/pizza_controller.py
@@ -3,3 +3,6 @@ from server.models.pizza import Pizza
 
 # Create a Blueprint for the pizza controller
 pizza_bp = Blueprint('pizza_bp', __name__)
+
+# Route to get all pizzas
+@pizza_bp.route('/pizzas', methods=['GET'])
