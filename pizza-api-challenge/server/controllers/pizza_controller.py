8797d8693db15458c5e144472c0e73f81aa6172c
Joykinuthia <joyrosekinuthia@gmail.com>
2025-06-16 23:28:23 +0300

    Feature: Get all pizzas

--- pizza-api-challenge/server/controllers/pizza_controller.py
+++ pizza-api-challenge/server/controllers/pizza_controller.py
@@ -6,3 +6,11 @@ pizza_bp = Blueprint('pizza_bp', __name__)
 
 # Route to get all pizzas
 @pizza_bp.route('/pizzas', methods=['GET'])
+# Get all pizzas
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    return jsonify([{
+        'id': p.id,
+        'name': p.name,
+        'ingredients': p.ingredients
+    } for p in pizzas])
